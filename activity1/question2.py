c7dfa0fbf6400a6948f0fbb9d06cd1cae3fe37bd
Bugguga <nat14660@hotmail.com>
2023-08-16 09:39:40 +0700

    update

--- activity1/question2.py
+++ activity1/question2.py
@@ -2,6 +2,8 @@ import hashlib
 
 lis = []
 words = []
+dic = dict()
+
 substitutions = {
     'o': '0',
     'l': '1',
@@ -13,22 +15,6 @@ def transform(word):
     lis = [0] * len(word)
     combi(len(word), lis, 0, word)
 
-
-# def dfs(l,w,i):
-#     if(i == len(l)):
-#         return
-#     if w[i].lower() in substitutions and l[i]==1:
-#         new_word = w[:i] + substitutions[w[i].lower()] + w[i+1:]
-#         words.append(new_word)
-#         l[i] = 2
-#         dfs(l,new_word,i+1)
-#     if l[i] == 1 or l[i] == 2:
-#         dfs(l,w,i+1)
-#     else :
-#         l[i]=1
-#         new_word = w[:i] + w[i].upper() + w[i+1:]
-#         words.append(new_word)
-#         dfs(l, new_word, i+1)
 
 def combi(n, sol, leng, w):
     if (leng < n):
@@ -59,5 +45,4 @@ for word in dictionary_words:
 
 
 for word in words:
-    if "d54cc1fe76f5186380a0939d2fc1723c44e8a5f7" == hashlib.sha1((word).encode('utf-8')).hexdigest() or "d54cc1fe76f5186380a0939d2fc1723c44e8a5f7" == hashlib.md5((word).encode('utf-8')).hexdigest():
-        print(word)
+    dic[word] = hashlib.sha1((word).encode('utf-8')).hexdigest()
